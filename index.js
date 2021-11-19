bb129c9ed96f0d0aa36354b4e1c2f266611c2908
Maria <mmatrozzo@gmail.com>
2021-11-18 21:30:21 -0500

    Passing Project

--- index.js
+++ index.js
@@ -1,18 +1,54 @@
-// fetch('https://covers.openlibrary.org/a/olid/OL229501A-S.jpg')
-// .then(response => response.json())
-// .then ((data) => {
-//     console.log(data)
-
-
-// });
-
-
 
 fetch('https://www.dnd5eapi.co/api/monsters/tarrasque/')
 .then(response => response.json())
 .then ((monster) => {
     console.log(monster)
+    
+    const inBox = document.querySelector('.stats-card')
+                console.log("Am I in the box?   ", inBox)
 
-    console.log(monster.actions[0])
+
+//click event
+    document.querySelector('button').addEventListener("click", revealList)
+
+    function revealList() {
+        console.log('click')
+        monster.actions.forEach(
+            (eachAttack) => {
+            const attackStats = document.createElement('li')
+                attackStats.classList.add('attack')
+                attackStats.textContent = eachAttack.name
+               inBox.append(attackStats)
+               console.log("This is attackStats:   ", attackStats)
+                
+            })
+    }
+    
+
+// Comment section stuff
+const commentContainer = document.querySelector('.comment-container')
+    console.log("Is this the add comments section?",   commentContainer)
+const commentSection = document.querySelector('#comments-list')
+        console.log("Does this show a comment?  ", commentSection)
+const commentTextData = document.querySelector('#post-comment')
+    console.log("Is this the textbox??   ", commentTextData)
+
+
+commentContainer.addEventListener("submit", (e) => {
+    e.preventDefault()
+    console.log(commentTextData.value)
+    const newComment = document.createElement('li')
+    newComment.classList.add('comments')
+        newComment.textContent = commentTextData.value
+    e.target.reset()
+    commentSection.append(newComment)
+})
+
+//focus event
+    commentTextData.addEventListener("focus", () => {
+        document.querySelector('#post-comment').style.backgroundColor="pink";
+    })
+
 
 });
+// ^^end of fetch
